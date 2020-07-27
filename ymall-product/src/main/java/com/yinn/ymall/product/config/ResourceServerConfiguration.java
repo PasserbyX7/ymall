0e66355cf058889dd80fe494153e01eee1e0f73c
Passerby <756635176@qq.com>
2020-07-27 13:47:40 +0800

    配置跨域

--- ymall-product/src/main/java/com/yinn/ymall/product/config/ResourceServerConfiguration.java
+++ ymall-product/src/main/java/com/yinn/ymall/product/config/ResourceServerConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter{
@@ -25,15 +28,27 @@ public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter{
 					.antMatchers(HttpMethod.POST, "/**").permitAll()
 					.antMatchers(HttpMethod.PUT, "/**").permitAll()
 					.antMatchers(HttpMethod.DELETE, "/**").permitAll()
-					.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+					.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
 					.anyRequest().permitAll()
 			)
             .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-            .cors().disable();
+            .cors();
     }
 
     @Bean
     JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");//修改为添加而不是设置，* 最好改为实际的需要，我这是非生产配置，所以粗暴了一点
+        configuration.addAllowedMethod("*");//修改为添加而不是设置
+        configuration.addAllowedHeader("*");//这里很重要，起码需要允许 Access-Control-Allow-Origin
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
