751d96dd76694df2ed08a02a697445eebb23be58
Passerby <756635176@qq.com>
2020-07-27 12:46:07 +0800

    修改跨域支持方式

--- ymall-product/src/main/java/com/yinn/ymall/product/config/MvcConfiguration.java
+++ ymall-product/src/main/java/com/yinn/ymall/product/config/MvcConfiguration.java
@@ -1,11 +1,11 @@
-package com.yinn.ymall.product.config;
+// package com.yinn.ymall.product.config;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class MvcConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("**").allowedOrigins("*");
-    }
-}
+// public class MvcConfiguration implements WebMvcConfigurer {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("**").allowedOrigins("*");
+//     }
+// }
