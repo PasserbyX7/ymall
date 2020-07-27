fa0619fe3a4fa1a92f3629adf36215c2fd4b7fd0
Passerby <756635176@qq.com>
2020-07-27 12:39:43 +0800

    配置跨域

---
+++ ymall-product/src/main/java/com/yinn/ymall/product/config/MvcConfiguration.java
@@ -0,0 +1,11 @@
+package com.yinn.ymall.product.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("**").allowedOrigins("*");
+    }
+}
