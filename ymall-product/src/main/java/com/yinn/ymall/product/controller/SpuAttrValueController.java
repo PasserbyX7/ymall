0215407388a00c0e08b618a5f523f1cb87c2eae9
Passerby <756635176@qq.com>
2020-08-02 16:15:02 +0800

    新增SpuAttrValueController接口

--- ymall-product/src/main/java/com/yinn/ymall/product/controller/SpuAttrValueController.java
+++ ymall-product/src/main/java/com/yinn/ymall/product/controller/SpuAttrValueController.java
@@ -7,8 +7,10 @@ import com.yinn.ymall.product.entity.SpuAttrValue;
 import com.yinn.ymall.product.service.SpuAttrValueService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,33 @@ public class SpuAttrValueController {
 
     @Autowired
     private SpuAttrValueService spuAttrValueService;
+
+    @ApiOperation("spu属性查询")
+    @GetMapping("/{id}")
+    public R<SpuAttrValue> save(@PathVariable Long id) {
+        return R.ok(spuAttrValueService.getById(id));
+    }
+
+    @ApiOperation("spu属性新增")
+    @PostMapping
+    public R<Void> save(@RequestBody SpuAttrValue spuAttrValue) {
+        spuAttrValueService.save(spuAttrValue);
+        return R.ok();
+    }
+
+    @ApiOperation("spu属性修改")
+    @PutMapping
+    public R<Void> update(@RequestBody SpuAttrValue spuAttrValue) {
+        spuAttrValueService.updateById(spuAttrValue);
+        return R.ok();
+    }
+
+    @ApiOperation("spu属性删除")
+    @DeleteMapping("/{id}")
+    public R<Void> update(@PathVariable Long id) {
+        spuAttrValueService.removeById(id);
+        return R.ok();
+    }
 
     @ApiOperation("列出spu下所有spu属性")
     @GetMapping("/spus/{spuId}")
